4880258de06b70f5501ed98a99a7669193473058
Limar Sangalia <limarsangalia8@gmail.com>
2024-04-16 13:25:38 +0800

    Add files via upload

---
+++ src/components/Layout/FAQs.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export default function FAQs() {
+  return (
+    <>
+      <div className='flex flex-col gap-4 my-8'>
+        <h1 className='text-4xl font-semibold'>Frequently Asked Questions</h1>
+        <p className='text-black font-regular text-lg text-center'>Find quick answers to common queries about our servicer, process and more. We’re here to help you understand everything you need.</p>
+        <p className='text-black font-regular text-lg text-center'>Can’t you find what your looking for? Check our full documention.</p>
+      </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-auto my-10'>
+        <div className='relative border-solid border-gray-200 border-2 rounded-sm max-w-xs p-2 text-left'>
+          <div className='absolute italic -top-5 left-0 bg-black text-white rounded-full w-6 h-6 text-center font-bold'>i</div>
+          <h1 className='text-lg font-semibold'>What is NCIP in the Philippines?</h1>
+          <p className='text-black'>
+            The National Commission on Indigenous Peoples (NCIP) is the agency of the national government of the Philippines that is responsible for protecting the rights of the indigenous peoples of the Philippines. The commission is composed of seven commissioners.
+          </p>
+        </div>
+        
+        <div className='relative border-solid border-gray-200 border-2 rounded-sm max-w-xs p-2 text-left'>
+          <div className='absolute italic -top-5 left-0 bg-black text-white rounded-full w-6 h-6 text-center font-bold'>i</div>
+          <h1 className='text-lg font-semibold'>What is NCIP in the Philippines?</h1>
+          <p className='text-black'>
+            The National Commission on Indigenous Peoples (NCIP) is the agency of the national government of the Philippines that is responsible for protecting the rights of the indigenous peoples of the Philippines. The commission is composed of seven commissioners.
+          </p>
+        </div>
+
+        <div className='relative border-solid border-gray-200 border-2 rounded-sm max-w-xs p-2 text-left'>
+          <div className='absolute italic -top-5 left-0 bg-black text-white rounded-full w-6 h-6 text-center font-bold'>i</div>
+          <h1 className='text-lg font-semibold'>What is NCIP in the Philippines?</h1>
+          <p className='text-black'>
+            The National Commission on Indigenous Peoples (NCIP) is the agency of the national government of the Philippines that is responsible for protecting the rights of the indigenous peoples of the Philippines. The commission is composed of seven commissioners.
+          </p>
+        </div>
+
+        <div className='relative border-solid border-gray-200 border-2 rounded-sm max-w-xs p-2 text-left'>
+          <div className='absolute italic -top-5 left-0 bg-black text-white rounded-full w-6 h-6 text-center font-bold'>i</div>
+          <h1 className='text-lg font-semibold'>What is NCIP in the Philippines?</h1>
+          <p className='text-black'>
+            The National Commission on Indigenous Peoples (NCIP) is the agency of the national government of the Philippines that is responsible for protecting the rights of the indigenous peoples of the Philippines. The commission is composed of seven commissioners.
+          </p>
+        </div>
+        
+        <div className='relative border-solid border-gray-200 border-2 rounded-sm max-w-xs p-2 text-left'>
+          <div className='absolute italic -top-5 left-0 bg-black text-white rounded-full w-6 h-6 text-center font-bold'>i</div>
+          <h1 className='text-lg font-semibold'>What is NCIP in the Philippines?</h1>
+          <p className='text-black'>
+            The National Commission on Indigenous Peoples (NCIP) is the agency of the national government of the Philippines that is responsible for protecting the rights of the indigenous peoples of the Philippines. The commission is composed of seven commissioners.
+          </p>
+        </div>
+
+        <div className='relative border-solid border-gray-200 border-2 rounded-sm max-w-xs p-2 text-left'>
+          <div className='absolute italic -top-5 left-0 bg-black text-white rounded-full w-6 h-6 text-center font-bold'>i</div>
+          <h1 className='text-lg font-semibold'>What is NCIP in the Philippines?</h1>
+          <p className='text-black'>
+            The National Commission on Indigenous Peoples (NCIP) is the agency of the national government of the Philippines that is responsible for protecting the rights of the indigenous peoples of the Philippines. The commission is composed of seven commissioners.
+          </p>
+        </div>
+      </div>
+      
+    </>
+  )
+}
