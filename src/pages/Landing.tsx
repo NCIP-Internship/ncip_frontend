b690f1d9feac6b461e50f066b1df32fbd2f8b35f
apacjessie <apacjessie@gmail.com>
2024-04-16 19:39:04 +0800

    chore: import FAQs, Streamlined and Footer

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,15 +1,13 @@
+import FAQs from "@/components/Templates/FAQs";
+import Streamlined from "@/components/Templates/Streamlined";
+import Footer from "@/components/Templates/Footer";
+
 const LandingPage = () => {
   return (
     <>
-      <h1
-        className="font-bold text-xl md:text-2xl lg:text-3xl
-      tracking-wide"
-      >
-        Vite + React + TypeScript + Tailwindcss
-      </h1>
-      <p className="tracking-wide mt-2 text-base md:text-xl text-gray-400">
-        react-router-dom + ts path configuration
-      </p>
+      <Streamlined />
+      <FAQs />
+      <Footer />
     </>
   );
 };
