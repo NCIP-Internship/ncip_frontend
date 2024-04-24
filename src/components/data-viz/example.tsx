dd4fba30c26bc8c42c28f32214a1ad20b58f65e5
Leenard <lolleenard0@gmail.com>
2024-04-24 21:12:09 +0800

    feat: table init

---
+++ src/components/data-viz/example.tsx
@@ -0,0 +1,14 @@
+// Example page to demonstrate how to implement data-table w/ cva
+
+const Example = () => {
+  return (
+    <>
+      <h1 className="text-2xl tracking-tight my-20 font-bold">
+        Testing table component
+      </h1>
+      <div>Table here</div>
+    </>
+  );
+};
+
+export default Example;
--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,8 +1,13 @@
+import Example from "@/components/data-viz/example";
 import PublicPage from "../pages/Public";
 
 export const publicRoutes = [
   {
     path: "/public",
     element: <PublicPage />,
+  },
+  {
+    path: "test/data-viz",
+    element: <Example />,
   },
 ];
