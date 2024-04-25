d109de74d79140b96f755bb5fd95e71ef43dd78a
Charles Matthew Golo <matthewgolo06@gmail.com>
2024-04-25 15:20:02 +0800

    New Commit

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,16 +1,9 @@
-import Ipgroup from "../components/Ipgroup";
-import NcipLogo from "../components/NcipLogo";
+import { Ipgroup } from "@/components/Ipgroup/Ipgroup";
 
 const LandingPage = () => {
   return (
     <>
-      <h1 className="text-4xl mb-8 float-left">
-        Indigenous Culture Communities/Indigenous Peoples(ICCs/IPs)
-      </h1>
-      <div className="container mx-auto flex">
-        <Ipgroup />
-        <NcipLogo></NcipLogo>
-      </div>
+      <Ipgroup />
     </>
   );
 };
