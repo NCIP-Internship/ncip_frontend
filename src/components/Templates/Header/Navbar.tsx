34abefe5261e928161b55fd509fb4874fea3288e
Charles Matthew Golo <matthewgolo06@gmail.com>
2024-04-26 17:07:01 +0800

    new commit

--- src/components/Templates/Header/Navbar.tsx
+++ src/components/Templates/Header/Navbar.tsx
@@ -1,74 +1,105 @@
-import { useState } from "react";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import Button from "./button";
-import NavLinks from "./Navlinks";
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
   return (
-    <nav className="bg-primary">
-      <div className="flex items-center font-medium justify-around">
-        <div className="z-50 p-5 md:w-auto w-full flex justify-between">
-          <h1 className="text-white font-Inter font-extrabold text-4xl">
-            GOVPH
-          </h1>
-          <div
-            className={`${open ? "text-black" : "text-white"} text-3xl md:hidden`}
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <AiOutlineClose /> : <AiOutlineMenu />}
-          </div>
-        </div>
-        <ul className="md:flex hidden items-center gap-8 font-Inter text-white font-semibold">
-          <li>
-            <Link to="/" className="py-7 px-3 inline-block">
-              Home
-            </Link>
-          </li>
-          <NavLinks />
-          <li>
-            <Link to="/" className="py-7 px-3 inline-block">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="py-7 px-3 inline-block">
-              FAQs
-            </Link>
-          </li>
-        </ul>
-        <div className="md:block hidden">
-          <Button />
-        </div>
+    <nav className="bg-primary sticky top-0 z-50 w-full max-h-24 h-auto p-8 flex items-center justify-around">
+        <h1 className="font-extrabold text-4xl text-white">GOVPH</h1>
+        <ul className="text-white flex gap-20">
+          <li className="hover:font-semibold"><Link to="">Home</Link></li>
 
-        {/* for mob nav */}
-        <ul
-          className={`md:hidden z-10 px-10 font-Inter font-semibold bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24  duration-500 ${open ? "left-0" : "left-[-100%]"}`}
-        >
-          <li className="flex item-center">
-            <Link to="/" className="py-7 px-3 inline-block">
-              Home
-            </Link>
+          <li className="hover:font-semibold">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Link to="#" className="hover:font-semibold">Services</Link>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent className="w-56 ml-40 mt-8 p-2">
+                
+                <DropdownMenuLabel className="font-extrabold">Categories</DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger><span className="font-semibold">COC</span></DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent className="ml-2">
+
+                        <DropdownMenuItem><span>Application Process</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Requirements</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Approved COC</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Guidelines</span></DropdownMenuItem>
+
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+
+
+                  <DropdownMenuSub>
+                  <DropdownMenuSubTrigger><span>IPO/IPS</span></DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent className="ml-2">
+
+                        <DropdownMenuItem><span>Registration Process</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Requirements</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Registered IPO</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Library Documentation</span></DropdownMenuItem>
+                        
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+
+
+                  <DropdownMenuSub>
+                  <DropdownMenuSubTrigger><span>IPMR</span></DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent className="ml-2">
+
+                        <DropdownMenuItem><span>Selection Process</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>IPMR List</span></DropdownMenuItem>
+
+                        <DropdownMenuItem><span>Guideline</span></DropdownMenuItem>
+        
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+
+                  <DropdownMenuItem>
+                    <span>IP Group</span>
+                  </DropdownMenuItem>
+      
+              </DropdownMenuContent>
+            </DropdownMenu>
           </li>
-          <NavLinks />
-          <li className="flex item-center">
-            <Link to="/" className="py-7 px-3 inline-block">
-              About
-            </Link>
-          </li>
-          <li className="flex item-center">
-            <Link to="/" className="py-7 px-3 inline-block">
-              FAQs
-            </Link>
-          </li>
-          <div className="py-5 border-inherit flex align-center justify-center font-bold">
-            <Button />
-          </div>
+
+          <li className="hover:font-semibold"><Link to="">About</Link></li>
+          <li className="hover:font-semibold"><Link to="">FAQs</Link></li>
         </ul>
-      </div>
+
+        <Button variant="outline" className="font-Inter border-2  px-6 py-2 rounded-full text-white bg-white-500 shadow-md hover:text-black hover:bg-white" >Open</Button>   
+        
     </nav>
+
   );
 };
 
