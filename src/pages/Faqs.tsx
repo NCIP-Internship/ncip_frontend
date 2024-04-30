9d0140dfd5cd0dff2870b9fc6cafac42b4d44192
doyoon1 <limarextraemail@gmail.com>
2024-04-30 18:08:41 +0800

    first commit

---
+++ src/pages/Faqs.tsx
@@ -0,0 +1,98 @@
+import Navbar from '@/components/Templates/Header/Navbar'
+
+const Faqs = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='bg-primary text-white py-8 px-4 md:py-20 md:px-4'>
+        <div className='flex flex-col md:flex-row justify-center md:justify-around items-center gap-6'>
+          <div className='w-full md:w-2/5 flex flex-col items-center md:items-start gap-4'>
+            <h1 className='text-2xl text-center md:text-left'>Having Questions About Us? We Have Just The Right Answer For You</h1>
+            <input className='w-full h-8 rounded-md border-2 px-8' type="text" placeholder='Search articles [ex. reset password, application process]' />
+          </div>
+          <div className='flex justify-center items-center gap-6'>
+            <h1 className='text-xl text-center md:text-center'>
+              National Commission of<br/> Indigenous Peoples
+            </h1>
+            <img className="h-20 w-20 md:h-32 md:w-32" src="logo.png" alt="Logo" />
+          </div>
+        </div>
+      </div>
+
+      <div className='text-black py-8 px-4 md:py-20 md:px-4'>
+        <div className='flex flex-col md:flex-row gap-8 md:gap-20 text-left justify-center items-center md:items-start'>
+          <div className='flex flex-col gap-4 border-2 rounded-md p-6 md:p-10 w-full md:w-2/5'>
+            <h1 className='text-xl font-semibold'>Frequently Asked Questions</h1>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>What is NCIP in the Philippines?</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>What is NCIP in the Philippines?</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>What is NCIP in the Philippines?</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>What is NCIP in the Philippines?</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>What is NCIP in the Philippines?</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>What is NCIP in the Philippines?</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+              </button>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-4 border-2 rounded-md p-6 md:p-10 w-full md:w-2/5'>
+            <h1 className='text-xl font-semibold'>Categories</h1>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>Guide: Application for COC</p>
+              <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-right"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="m12 16 4-4-4-4"/></svg>              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>Guide: Application for COC</p>
+              <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-right"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="m12 16 4-4-4-4"/></svg>              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>Guide: Application for COC</p>
+              <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-right"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="m12 16 4-4-4-4"/></svg>              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>Guide: Application for COC</p>
+              <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-right"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="m12 16 4-4-4-4"/></svg>              </button>
+            </div>
+            <div className='flex justify-between text-sm border-b-2 pb-2'>
+              <p>Guide: Application for COC</p>
+              <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-right"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="m12 16 4-4-4-4"/></svg>              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Faqs
