e347ded39b25855ea69b26b0e1c30058a8be1a93
apacjessie <apacjessie@gmail.com>
2024-06-20 17:24:51 +0800

    chore: removed unused component

--- src/components/Templates/Header/button.tsx
+++
@@ -1,9 +0,0 @@
-const Button = () => {
-    return (
-      <button className="font-primary border-2 border-slate-700  px-6 py-2 rounded-full text-black bg-white hover:text-white hover:bg-slate-800 shadow-md">
-        Sign In
-      </button>
-    );
-  };
-  
-  export default Button;
