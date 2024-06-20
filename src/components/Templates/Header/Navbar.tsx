fbc00998994c806ddbcdbb18421697179b8ffa53
Charles Matthew Golo <matthewgolo06@gmail.com>
2024-06-20 23:49:06 +0800

    dddd

--- src/components/Templates/Header/Navbar.tsx
+++ src/components/Templates/Header/Navbar.tsx
@@ -13,8 +13,15 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-
 const Navbar = () => {
+
+  const handleScroll = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <nav className="bg-primary sticky top-0 z-50 w-full max-h-24 h-auto p-8 flex items-center justify-around">
       <h1 className="font-extrabold text-4xl text-white">GOVPH</h1>
@@ -30,75 +37,48 @@ const Navbar = () => {
             </DropdownMenuTrigger>
 
             <DropdownMenuContent className="w-56 ml-40 mt-2 p-2">
-
               <DropdownMenuLabel className="font-extrabold">Categories</DropdownMenuLabel>
-
               <DropdownMenuSeparator />
-
               <DropdownMenuSub>
                 <DropdownMenuSubTrigger><span className="font-semibold">COC</span></DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
                   <DropdownMenuSubContent className="ml-2">
-
                     <DropdownMenuItem><Link to="/coc/application-process">Application Process</Link></DropdownMenuItem>
-
                     <DropdownMenuItem><Link to="/coc/requirements">Requirements</Link></DropdownMenuItem>
-
                     <DropdownMenuItem><span>Approved COC</span></DropdownMenuItem>
-
                     <DropdownMenuItem><Link to="/coc/guidelines">Guidelines</Link></DropdownMenuItem>
-
                   </DropdownMenuSubContent>
                 </DropdownMenuPortal>
               </DropdownMenuSub>
-
-
               <DropdownMenuSub>
                 <DropdownMenuSubTrigger><span>IPO/IPS</span></DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
                   <DropdownMenuSubContent className="ml-2">
-
                     <DropdownMenuItem><Link to="/ipoips/registration_process">Registration Process</Link></DropdownMenuItem>
-
                     <DropdownMenuItem><Link to="/ipoips/requirements">Requirements</Link></DropdownMenuItem>
-
                     <DropdownMenuItem><span>Registered IPO</span></DropdownMenuItem>
-
                     <DropdownMenuItem><span>Library Documentation</span></DropdownMenuItem>
-
                   </DropdownMenuSubContent>
                 </DropdownMenuPortal>
               </DropdownMenuSub>
-
-
               <DropdownMenuSub>
                 <DropdownMenuSubTrigger><span>IPMR</span></DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
                   <DropdownMenuSubContent className="ml-2">
-
                     <DropdownMenuItem><Link to="/ipmr/selection_process">Selection Process</Link></DropdownMenuItem>
-
                     <DropdownMenuItem><span>IPMR List</span></DropdownMenuItem>
-
                     <DropdownMenuItem><Link to="/ipmr/guidelines">Guideline</Link></DropdownMenuItem>
-
                   </DropdownMenuSubContent>
                 </DropdownMenuPortal>
               </DropdownMenuSub>
-
-              <DropdownMenuItem>
-                <span>IP Group</span>
-              </DropdownMenuItem>
-
+              <DropdownMenuItem><span>IP Group</span></DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </li>
-
-        <li className="hover:font-semibold w-8"><Link to="">About</Link></li>
-        <li className="hover:font-semibold w-8"><Link to="">FAQs</Link></li>
+        <li className="hover:font-semibold w-8"><Link to="#" onClick={() => handleScroll('about')}>About</Link></li>
+        <li className="hover:font-semibold w-8"><Link to="#" onClick={() => handleScroll('faqs')}>FAQs</Link></li>
       </ul>
-
-      <Button variant="outline" className="font-Inter border-2  px-6 py-2 rounded-full text-white bg-white-500 shadow-md hover:text-black hover:bg-white" >Sign In</Button>
+      <Button variant="outline" className="font-Inter border-2  px-6 py-2 rounded-full text-white bg-white-500 shadow-md hover:text-black hover:bg-white">Sign In</Button>
     </nav>
   );
 };
