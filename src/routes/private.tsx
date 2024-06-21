0ff99af8ac857bb10099dbbfbdf37901cb6f015b
apacjessie <apacjessie@gmail.com>
2024-06-21 11:50:25 +0800

    feat: add private routes for coc/application_process

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,10 +1,15 @@
 import AuthenticatedLayout from "@/components/Layout/AuthenticatedLayout";
+import Form from "@/pages/private/COC/Form";
 import Dashboard from "@/pages/private/Dashboard";
 
 const children = [
   {
     path: 'dashboard',
     element: <Dashboard />
+  },
+  {
+    path: 'coc/application_process',
+    element: <Form />
   }
 ]
 
