0ba945616b4129f051e6192ae63da7d8e7403052
apacjessie <apacjessie@gmail.com>
2024-06-21 10:23:37 +0800

    chore<style>: update style

--- src/components/Layout/AuthenticatedLayout.tsx
+++ src/components/Layout/AuthenticatedLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../Templates/Sidebar"
+import HeaderAuthorized from '../Templates/Header/Header[authorized]'
 
 const AuthenticatedLayout = () => {
   return (
-    <main className="flex">
+    <main className="flex w-full">
       <Sidebar />
-      <Outlet />
+      <section className="w-full">
+        <HeaderAuthorized />
+        <Outlet />
+      </section>
     </main>
   )
 }
